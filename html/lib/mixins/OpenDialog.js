2de5150f64ab5933ce024a378b68b924b8ba89fc
cqb325@163.com <cqb325@163.com>
2016-08-29 17:02:44 +0800

    dialog 打开方式mixin

---
+++ html/lib/mixins/OpenDialog.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    componentDidUpdate: function () {
+        for (let key in this.refs) {
+            let ref = this.refs[key];
+            if (ref.refs && ref.props["data-toggle"] == "dialog") {
+                let listener = () => {
+                    let target = ref.props["data-target"];
+                    let targetRef = this.refs[target];
+                    if (targetRef.open) {
+                        targetRef.open();
+                        return;
+                    }
+                };
+                ref.un("click", listener);
+                ref.on("click", listener);
+            }
+        }
+    }
+};
